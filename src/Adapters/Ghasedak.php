7a411f58b67171aafb99db79008f6eceebcf6774
Kamyar <kamyar.ea@gmail.com>
2019-09-04 17:54:36 +0430

    Ghasedak panel adapter was added

---
+++ src/Adapters/Ghasedak.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tartan\IranianSms\Adapter;
+
+use Ghasedak\GhasedakApi;
+
+class Ghasedak extends AdapterAbstract implements AdapterInterface
+{
+
+    private $credential = [
+        'api_key'   => '',
+        'sender'   => ''
+    ];
+
+    public function __construct()
+    {
+        $this->credential['api_key']   = config('iranian_sms.ghasedak.api_key');
+        $this->credential['sender'] = config('iranian_sms.ghasedak.sender');
+    }
+
+    public function send(String $number, String $message)
+    {
+
+        $number = $this->filterNumber($number);
+
+        $api = new GhasedakApi($this->credential['api_key']);
+
+        return $api->SendSimple(
+            $number,
+            $message,
+            $this->credential['sender']
+        );
+
+    }
+}
