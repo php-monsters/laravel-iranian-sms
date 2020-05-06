9b02a4a2818480a8572e9c2a64e5d9ccc593bcf9
davoudb <d.bolhasani@ihome.ir>
2020-05-06 19:04:26 +0430

    fix discord adapter

--- src/Adapter/Discord.php
+++ src/Adapter/Discord.php
@@ -15,13 +15,11 @@ class Discord extends AdapterAbstract implements AdapterInterface
     {
         $number = $this->filterNumber($number);
 
-        $data = json_encode(['content' => "To: $number - Message: $message"]);
+        $data = ['content' => "To: $number - Message: $message"];
 
         $ch = curl_init($this->url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, array('Content-Type: application/json'));
         $data = curl_exec($ch);
 
         if(curl_errno($ch)){
